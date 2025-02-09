199a3eb145ed0e63bc0a77eed2a264e790f39967
asmitmans <a.smitmans@gmail.com>
2025-02-09 02:16:38 -0300

    Eliminar archivos de compilación del repositorio y respetar .gitignore

--- components/tasks/task_http_post.h
+++
@@ -1,6 +0,0 @@
-#ifndef TASK_HTTP_POST_H
-#define TASK_HTTP_POST_H
-
-void task_http_post(void *pvParameters);
-
-#endif // TASK_HTTP_POST_H
--- components/tasks/task_sensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef TASK_SENSOR_H
-#define TASK_SENSOR_H
-
-// Declaración de la tarea de lectura del sensor
-void task_sensor_read(void *pvParameters);
-
-#endif // TASK_SENSOR_H
